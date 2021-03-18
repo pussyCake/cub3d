5edf5e32f7d282a4e0af6db4dc84eb5f697160d6
Proteus Antigone <pantigon@va-j3.kzn.21-school.ru>
2021-03-18 15:58:17 +0300

    gg

---
+++ cub_lodev/srcs/libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pantigon <pantigon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/31 19:49:03 by pantigon          #+#    #+#             */
+/*   Updated: 2020/11/01 13:12:43 by pantigon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *arr, int c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)arr)[i] == (unsigned char)c)
+			return (((void *)arr) + i);
+		i++;
+	}
+	return (NULL);
+}
